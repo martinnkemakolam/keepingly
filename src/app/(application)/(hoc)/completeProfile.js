572f2c81361fab1959b62896273b27e840c5d157
Nkemakolam martin <nkemakolam.martin@gmail.com>
2023-09-22 11:52:09 +0100

    moved indexdb upgrade func to layout

--- src/app/(application)/(hoc)/completeProfile.js
+++ src/app/(application)/(hoc)/completeProfile.js
@@ -133,6 +133,7 @@ export default function CompleteProfile() {
                 if (userData.result) {
                     setUserFormData(userData.result)   
                 }
+                
             }
         }
     }, [])
