a4c4669f4a54bfaf913d7895611915952e340a91
Onwukaike Martin <nkemakolam.martin@gmail.com>
2023-11-07 07:49:17 +0100

    switched property client side

--- src/app/@application/(pages)/property.js
+++ src/app/@application/(pages)/property.js
@@ -2,11 +2,28 @@
 import { useRouter } from "next/navigation"
 import TopBar from "../(container)/Topbar"
 import Table from "../(container)/table"
-export let Properties=({property})=>{
+import { useEffect, useState } from "react"
+import { api } from "@/app/keepinglyClientApi"
+export let Properties=()=>{
+    let [property, setProperty]=useState([])
     let router = useRouter()
     let btnFunc =()=>{
         router.push('./Properties/addproperty')
     }
+    useEffect(()=>{
+        (async()=>{
+            let tk = sessionStorage.getItem('kpuo')
+            const res = await api.get("/api/v2/get_property_user/", {
+                cache: "no-cache",
+                'headers': { 
+                    Authorization: `Bearer ${tk}` 
+                },
+                mode: 'cors'
+            })
+            console.log(res.data)
+            setProperty(res.data || [])
+        })()
+    }, [])
     return (
         <>
         {/* {console.log('this is your cookie in property',document.cookie)} */}
